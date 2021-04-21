89d96f532dfc4beae81eeb973ad97a8a6c7ee253
Adam Wong <adamwthedev@gmail.com>
2021-04-21 11:24:54 -0700

    added picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styled from 'styled-components';
+import Picture from './images/bruery.jpg';
 
 const AppDiv = Styled.div`
   @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
@@ -11,22 +12,33 @@ const AppDiv = Styled.div`
     margin-left: 24px;
 
     .date {
+      font-size: 24px;
       font-family: 'Amatic SC', cursive;
     }
 
     .greeting {
-      font-family: 'Dancing Script', cursive;
       font-size: 48px;
+      font-family: 'Dancing Script', cursive;
     }
   }
 
-  .picture {
-    text-align: center;
-  }
+  .body {
+    display: grid;
+    grid-template-columns: 35% 65%;;
 
-  .message {
-    margin: 48px 48px;
-    font-family: 'Amatic SC', cursive;
+    .picture {
+      margin-left: 24px;
+      border: 2px solid gray;
+      border-radius: 50%;
+      width: 300px;
+      height: 400px;
+    }
+  
+    .message {
+      margin: 96px 48px;
+      font-size: 24px;
+      font-family: 'Amatic SC', cursive;
+    }
   }
 `
 
@@ -34,22 +46,26 @@ const App = () => {
   return (
     <AppDiv>
       <header className="heading">
-        <p className="date">April 3rd 2021</p>
+        <p className="date">April 23rd, 2021</p>
         <p className="greeting">Happy Birthday Cue!</p>
       </header>
-      <section className="message">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Curabitur eu pretium metus. Suspendisse elementum, velit eu posuere ultricies, 
-        nisi sem tincidunt lectus, nec vulputate diam nunc placerat eros. Pellentesque 
-        molestie convallis ligula, maximus porttitor felis consectetur eu. Cras vel dolor ultricies,
-        blandit ante in, congue sapien. Vivamus ut porta nunc. Interdum et malesuada fames ac ante 
-        ipsum primis in faucibus. In a viverra lorem, vitae interdum dui. Quisque pharetra risus quis
-        efficitur blandit. Proin tristique elementum lectus, at blandit leo tincidunt convallis. Suspendisse 
-        potenti. Ut cursus ex vel lobortis lobortis. Morbi imperdiet erat sit amet cursus commodo. Suspendisse 
-        eros erat, pharetra sed nunc vulputate, finibus dapibus lacus. Quisque velit ipsum, facilisis et semper 
-        et, rhoncus vel ligula. Donec accumsan eu augue sed fermentum. Nullam posuere pharetra elit imperdiet tincidunt.
-      </section>
-      <div className="picture">INSERT PICTURE HERE</div>
+      <div className="body">
+        <div>
+          <img className="picture" src={Picture} alt="bruery-pic"/> 
+        </div>
+        <section className="message">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Curabitur eu pretium metus. Suspendisse elementum, velit eu posuere ultricies, 
+          nisi sem tincidunt lectus, nec vulputate diam nunc placerat eros. Pellentesque 
+          molestie convallis ligula, maximus porttitor felis consectetur eu. Cras vel dolor ultricies,
+          blandit ante in, congue sapien. Vivamus ut porta nunc. Interdum et malesuada fames ac ante 
+          ipsum primis in faucibus. In a viverra lorem, vitae interdum dui. Quisque pharetra risus quis
+          efficitur blandit. Proin tristique elementum lectus, at blandit leo tincidunt convallis. Suspendisse 
+          potenti. Ut cursus ex vel lobortis lobortis. Morbi imperdiet erat sit amet cursus commodo. Suspendisse 
+          eros erat, pharetra sed nunc vulputate, finibus dapibus lacus. Quisque velit ipsum, facilisis et semper 
+          et, rhoncus vel ligula. Donec accumsan eu augue sed fermentum. Nullam posuere pharetra elit imperdiet tincidunt.
+        </section>
+      </div>
     </AppDiv>
   );
 }
